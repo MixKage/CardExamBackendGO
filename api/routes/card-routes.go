f263691268470af84984f69a1d7a4837c01ea918
Alexey <aleksej.serbinov29@gmail.com>
2023-05-12 12:56:36 +0100

    fix: removed / and changed from api to server

--- api/routes/card-routes.go
+++ api/routes/card-routes.go
@@ -8,11 +8,11 @@ import (
 )
 
 var RegisterCardRoutes = func(router *mux.Router) {
-	router.HandleFunc("/v1/api/card/", controllers.CreateCard).Methods("POST")
-	router.HandleFunc("/v1/api/cardUserAuth/", controllers.CreateCardAuthUserOnly).Methods("POST")
-	router.HandleFunc("/v1/api/card/{CardId}", controllers.GetCardById).Methods("GET")
-	router.HandleFunc("/v1/api/signup/", controllers.SignUp).Methods("POST")
-	router.HandleFunc("/v1/api/signin/", controllers.SignIn).Methods("POST")
+	router.HandleFunc("/v1/server/card", controllers.CreateCard).Methods("POST")
+	router.HandleFunc("/v1/server/cardUserAuth", controllers.CreateCardAuthUserOnly).Methods("POST")
+	router.HandleFunc("/v1/server/card/{CardId}", controllers.GetCardById).Methods("GET")
+	router.HandleFunc("/v1/server/signup", controllers.SignUp).Methods("POST")
+	router.HandleFunc("/v1/server/signin", controllers.SignIn).Methods("POST")
 	router.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
